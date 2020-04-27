dd4294f4a21060aaa86e4916b65cc2693ee29330
Magomet Albakov <malbakov@gmail.com>
2020-04-26 20:07:35 -0400

    Initial commit with .gitignore

---
+++ TV-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TV-Test
+//
+//  Created by Tom Malary on 4/26/20.
+//  Copyright © 2020 YOUTIR LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
